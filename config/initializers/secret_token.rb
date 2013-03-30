f334d4d3b5548fff270c23d5c9c096d7416ff643
willnet <netwillnet@gmail.com>
2013-03-30 11:01:35 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeLoveKpt::Application.config.secret_key_base = '38284f3771b8bf2ab925e56d2ca68c717da47306ff7be3bbfe82292f8ffb35a4bfac38438f5c1616f841009b076bccdb37a5d285c552d46595a14a546df2ad6c'
